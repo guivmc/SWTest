219ccf562047b43d0c47d5ddf36efa7a15870ebd
guivmc <guivmc@hotmail.com>
2019-08-14 13:44:56 -0300

    Start

---
+++ santanderWay/santanderWay/src/main/java/com/santanderWay/santanderWay/SantanderWayApplication.java
@@ -0,0 +1,15 @@
+package com.santanderWay.santanderWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWayApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SantanderWayApplication.class, args);
+	}
+
+}
