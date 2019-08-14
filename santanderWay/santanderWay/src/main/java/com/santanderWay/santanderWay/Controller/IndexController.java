abdeea2c515b39e6d7b0105adeccada27206669c
Carvalhal <guilherme.carvalhal@accenture.com>
2019-08-14 15:29:23 -0300

    testetstets

--- santanderWay/santanderWay/src/main/java/com/santanderWay/santanderWay/Controller/IndexController.java
+++ santanderWay/santanderWay/src/main/java/com/santanderWay/santanderWay/Controller/IndexController.java
@@ -1,7 +1,11 @@
-package com.santanderWay.santanderWay;
+package com.santanderWay.santanderWay.Controller;
 
+import com.santanderWay.santanderWay.Model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController
@@ -11,5 +15,15 @@ public class IndexController
     public String index()
     {
         return "index";
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute("User") User user, BindingResult result)
+    {
+
+        System.out.println("Name:" + user.getName() +
+                "Password:" + user.getPassword());
+
+        return "redirect:index.html";
     }
 }
