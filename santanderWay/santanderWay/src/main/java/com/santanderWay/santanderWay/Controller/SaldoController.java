a2cfd85681aa6d6158c596c367e8967f1e37c8e7
guivmc <guivmc@hotmail.com>
2019-08-22 10:36:33 -0300

    FixedSaldoController

--- santanderWay/santanderWay/src/main/java/com/santanderWay/santanderWay/Controller/SaldoController.java
+++ santanderWay/santanderWay/src/main/java/com/santanderWay/santanderWay/Controller/SaldoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -20,23 +21,30 @@ public class SaldoController
     @Autowired
     private SaldoRepository saldoRepository;
 
+
     @RequestMapping(value="/addSaldo", method = RequestMethod.POST)
     public String addSaldo(@ModelAttribute("Saldo") Saldo saldo, HttpSession session)
     {
         if(session.getAttribute("userId") == null)
             return "view/index";
 
+        saldo.setIdCartao((long) session.getAttribute("cardId"));
+
+        session.removeAttribute("cardId");
+
         this.saldoRepository.save(saldo);
 
-        return "redirect:/addSaldo";
+        return "redirect:/home";
     }
 
-    @RequestMapping("/{idCard}")
-    public ModelAndView showSaldos(@PathVariable("idCard") long idCard)
+    @RequestMapping("/addSaldo?idCard={idCard}")
+    public String showSaldos(@PathVariable("idCard") long idCard, HttpServletRequest request)
     {
         System.out.println(idCard);
 
-        return new ModelAndView("view/saldo");
+        request.getSession().setAttribute("cardId", idCard);
+
+        return "view/saldo";
     }
 
 
